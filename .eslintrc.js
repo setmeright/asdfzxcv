70ebf8565d844b5e4b010e6f24cc69317e5781f0
Arseny M <mailit@arsm.me>
2020-06-26 19:55:12 +0300

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,8 @@ module.exports = {
   ],
   plugins: ["prettier"],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
 };
